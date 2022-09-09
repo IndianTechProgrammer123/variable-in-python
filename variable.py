253d8ae822e2042872c3ed97616df57abd0ddfd4
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-09 19:56:22 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+# How many things we can save in one variable in python 
+
+# 1. float
+# 2. integer
+# 3. data types
+# 4. string
+# 5. data structure
+
+a = 12.5
+print(a)
+b = 15 
+print(b)
+c = 18 , 15.9 
+print(c)
+d = "hello"
+print(d)
+e = [1,2,3,4]
+print(e)
+
+# a,b,c,d,e is variables
