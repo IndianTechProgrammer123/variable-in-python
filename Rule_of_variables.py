39aa764085def75e8790dafd635fc14d7b26e6de
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-10 20:16:59 +0530

    Add files via upload

---
+++ Rule_of_variables.py
@@ -0,0 +1,24 @@
+# Rules of Variables in Python
+
+# 1. You cannot start a variable with a digit
+# 15 = 'hello'
+
+# 2. You can start the variable from the alphabet itself 
+# a = 'hello'
+# a15 = 'hello'
+
+# 3. You cannot use symbol in a variable
+# A` = 'hello'
+# A~ = 'hello'
+# A! = 'hello'
+# A@ = 'hello'
+# A$ = 'hello'
+# A% = 'hello'
+# A^ = 'hello'
+# A& = 'hello'
+# A? = 'hello'
+# A/ = 'hello'
+# A< = 'hello'
+# A> = 'hello'
+# A: = 'hello'
+# A; = 'hello'
